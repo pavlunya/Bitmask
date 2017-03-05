d17fa621ad189218098fc9dbca09fec52330ced3
Anthony Kuske <anthonykuske@gmail.com>
2017-03-05 19:21:41 +0000

    Make mask iterable
    * Implement Iterator interface
    * Use static constants instead of operations
    * Ensure the mask value is positive
    * Additional tests relating to strict / non-strict mode

--- tests/MaskTest.php
+++ tests/MaskTest.php
@@ -8,6 +8,14 @@ class MaskTest extends \PHPUnit_Framework_TestCase
 {
     const TEST_MASK = 42;
 
+    /**
+     * @expectedException \Abibidu\Bit\MaskException
+     */
+    public function testConstructorFailsOnInvalidMask()
+    {
+        new Mask(-1);
+    }
+
     public function testGetter()
     {
         $mask = new Mask(self::TEST_MASK);
@@ -41,6 +49,22 @@ class MaskTest extends \PHPUnit_Framework_TestCase
     public function testIfHasFlag()
     {
         $mask = new Mask(Mask::EMPTY_MASK | Mask::FLAG_13);
+
+        $this->assertFalse($mask->has(Mask::FLAG_3));
+        $this->assertTrue($mask->has(Mask::FLAG_13));
+    }
+
+    public function testIfHasFlagWithoutEmpty()
+    {
+        $mask = new Mask(Mask::FLAG_13);
+
+        $this->assertFalse($mask->has(Mask::FLAG_3));
+        $this->assertTrue($mask->has(Mask::FLAG_13));
+    }
+
+    public function testIfHasFlagFromInteger()
+    {
+        $mask = new Mask(4096);
 
         $this->assertFalse($mask->has(Mask::FLAG_3));
         $this->assertTrue($mask->has(Mask::FLAG_13));
@@ -102,23 +126,136 @@ class MaskTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($mask->has(Mask::FLAG_21));
     }
 
-    /**
-     * @expectedException \Abibidu\Bit\MaskException
-     */
     public function testAlreadyPresentFlagAddition()
     {
-        $mask = new Mask(Mask::FLAG_13);
+        $mask = new Mask(Mask::FLAG_13, false);
 
+        // Should not throw an exception when not in strict mode.
         $mask->add(Mask::FLAG_13);
+
+        $this->assertSame(Mask::FLAG_13, $mask->get());
     }
 
     /**
      * @expectedException \Abibidu\Bit\MaskException
      */
+    public function testAlreadyPresentFlagAdditionInStrictMode()
+    {
+        $mask = new Mask(Mask::FLAG_13);
+
+        // Should throw an exception in strict mode.
+        $mask->add(Mask::FLAG_13);
+    }
+
     public function testAbsentFlagRemoval()
+    {
+        $mask = new Mask(Mask::EMPTY_MASK, false);
+
+        // Should not throw an exception when not in strict mode.
+        $mask->remove(Mask::FLAG_13);
+
+        $this->assertSame(Mask::EMPTY_MASK, $mask->get());
+    }
+
+    /**
+     * @expectedException \Abibidu\Bit\MaskException
+     */
+    public function testAbsentFlagRemovalInStrictMode()
     {
         $mask = new Mask();
 
+        // Should throw an exception in strict mode.
         $mask->remove(Mask::FLAG_13);
+    }
+
+    public function testGetFlags()
+    {
+        $mask = new Mask(Mask::FLAG_9 | Mask::FLAG_21);
+
+        $this->assertSame(
+            [
+                Mask::FLAG_9,
+                Mask::FLAG_21,
+            ],
+            $mask->getFlags()
+        );
+    }
+
+    public function testGetFlagsAfterAdd()
+    {
+        $mask = new Mask(Mask::FLAG_9 | Mask::FLAG_21);
+
+        $mask->add(Mask::FLAG_19);
+
+        $this->assertSame(
+            [
+                Mask::FLAG_9,
+                Mask::FLAG_19,
+                Mask::FLAG_21,
+            ],
+            $mask->getFlags()
+        );
+    }
+
+    public function testGetFlagsAfterRemove()
+    {
+        $mask = new Mask(Mask::FLAG_9 | Mask::FLAG_21);
+
+        $mask->remove(Mask::FLAG_21);
+
+        $this->assertSame(
+            [
+                Mask::FLAG_9,
+            ],
+            $mask->getFlags()
+        );
+    }
+
+    public function testIterator()
+    {
+        $mask = new Mask(Mask::FLAG_9 | Mask::FLAG_21);
+
+        $output = [];
+        foreach ($mask as $flag) {
+            $output[] = $flag;
+        }
+
+        $this->assertSame(
+            [
+                Mask::FLAG_9,
+                Mask::FLAG_21
+            ],
+            $output
+        );
+    }
+
+    public function testIteratorCurrent()
+    {
+        $mask = new Mask(Mask::FLAG_9 | Mask::FLAG_21);
+
+        $this->assertSame(Mask::FLAG_9, $mask->current());
+        $this->assertSame(0, $mask->key());
+    }
+
+    public function testIteratorNext()
+    {
+        $mask = new Mask(Mask::FLAG_9 | Mask::FLAG_21);
+
+        $mask->next();
+        $this->assertSame(Mask::FLAG_21, $mask->current());
+        $this->assertSame(1, $mask->key());
+
+        $mask->next();
+        $this->assertNull($mask->current());
+        $this->assertSame(2, $mask->key());
+    }
+
+    public function testIteratorResetNext()
+    {
+        $mask = new Mask(Mask::FLAG_9 | Mask::FLAG_21);
+
+        $mask->next();
+        $mask->rewind();
+        $this->assertSame(Mask::FLAG_9, $mask->current());
     }
 }
